ad8aca69174d5a5adc74135671fc7004981e98a0
GimeVidela <gimevmprs@gmail.com>
2019-11-25 13:25:11 -0300

    Base de TP Final

---
+++ TPFinal/TPFinal/GeneradorNumAleatorios.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFinal
+{
+    class GeneradorNumAleatorios
+    {
+        double primerValor, x1, x2;
+        Random aleatorio = new Random();
+        public double GenerarAleatorio()
+        {
+
+            while (primerValor == x2)
+            {
+                primerValor = aleatorio.Next(0, 9998);
+                x1 = primerValor;
+            }
+
+            x2 = (67 * x1 + 71) % 9999;
+            x1 = x2;
+            return x2 / 10000;
+
+
+        }
+
+        public TimeSpan convertirSegundosHorasMinutos(double minutos)
+        {
+            int segundos = Convert.ToInt32(minutos * 60);
+            int hor = 0;
+            int min = 0;
+            int seg = 0;
+            hor = (segundos / 3600);
+            min = ((segundos - hor * 3600) / 60);
+            seg = segundos - (hor * 3600 + min * 60);
+            return TimeSpan.Parse(hor + ":" + min + ":" + seg);
+        }
+        public TimeSpan convertirSegundosHorasMinutosPromedio(double minutos)
+        {
+
+            int segundos = Convert.ToInt32(minutos);
+            int hor = 0;
+            int min = 0;
+            int seg = 0;
+            hor = (segundos / 3600);
+            min = ((segundos - hor * 3600) / 60);
+            seg = segundos - (hor * 3600 + min * 60);
+
+            return TimeSpan.Parse(hor + ":" + min + ":" + seg);
+        }
+
+        public Double convertirEnDouble(TimeSpan tiempo)
+        {
+            return tiempo.TotalSeconds;
+        }
+    }
+}
---
+++ TPFinal/TPFinal/Inicio.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TPFinal
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.imgIcono = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_inicio = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgIcono
+            // 
+            this.imgIcono.Image = ((System.Drawing.Image)(resources.GetObject("imgIcono.Image")));
+            this.imgIcono.Location = new System.Drawing.Point(12, 12);
+            this.imgIcono.Name = "imgIcono";
+            this.imgIcono.Size = new System.Drawing.Size(202, 42);
+            this.imgIcono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgIcono.TabIndex = 28;
+            this.imgIcono.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 54);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Simulación ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(488, 31);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Trabajo Práctico Final - Disquería EDEN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(311, 27);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Gimena Videla (Legajo 66984)";
+            // 
+            // btn_inicio
+            // 
+            this.btn_inicio.Location = new System.Drawing.Point(212, 181);
+            this.btn_inicio.Name = "btn_inicio";
+            this.btn_inicio.Size = new System.Drawing.Size(75, 23);
+            this.btn_inicio.TabIndex = 32;
+            this.btn_inicio.Text = "Inicio";
+            this.btn_inicio.UseVisualStyleBackColor = true;
+            this.btn_inicio.Click += new System.EventHandler(this.btn_inicio_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 216);
+            this.Controls.Add(this.btn_inicio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgIcono);
+            this.Name = "Inicio";
+            this.Text = "TP Final - Disquería EDEN";
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgIcono;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_inicio;
+    }
+}
+
